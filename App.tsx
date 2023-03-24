512f844e03e0e0c6296cd804ef4d22adf87a8fe2
Sina <SinaKordestani@gmail.com>
2023-03-24 23:48:14 +0430

    Change package name

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { RN_Responsive } from 'rn-responsive-view';
+import { RN_Responsive } from 'react-native-responsive-font';
 const rn_responsive = new RN_Responsive();
 const rsp = rn_responsive.GetRF;
 
